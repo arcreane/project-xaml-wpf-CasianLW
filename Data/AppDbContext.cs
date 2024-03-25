9ffda30ce80c692082f434be44a3ef4c034e706e
CasianLW <ciorbcasian@gmail.com>
2024-03-25 16:00:40 +0100

    db helper for user crud

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,17 +3,42 @@
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext() : base("name=DefaultConnection") // This should match the name of the connection string in your config file
+    public AppDbContext() : base("name=DefaultConnection")
     {
     }
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-    //}
 
     public DbSet<User> Users { get; set; }
     public DbSet<Tool> Tools { get; set; }
     public DbSet<Category> Categories { get; set; }
-    // ... other DbSets for your models
+
+    // Call this method to add a user
+    public void AddUser(User user)
+    {
+        Users.Add(user);
+        SaveChanges();
+    }
+
+    // Call this method to get a user
+    public User GetUser(int id)
+    {
+        return Users.Find(id);
+    }
+
+    // Call this method to update a user
+    public void UpdateUser(User user)
+    {
+        Entry(user).State = EntityState.Modified;
+        SaveChanges();
+    }
+
+    // Call this method to delete a user
+    public void DeleteUser(int id)
+    {
+        var user = Users.Find(id);
+        if (user != null)
+        {
+            Users.Remove(user);
+            SaveChanges();
+        }
+    }
 }
