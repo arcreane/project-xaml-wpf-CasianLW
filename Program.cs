69c516a19623acdacac6e5720e4acd079d135b61
CasianLW <ciorbcasian@gmail.com>
2024-03-25 15:16:14 +0100

    link db to program cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Data.Entity;
+
 namespace ToolsManagerApp
 {
     internal static class Program
@@ -8,6 +10,11 @@ namespace ToolsManagerApp
         [STAThread]
         static void Main()
         {
+
+            Database.SetInitializer(new DatabaseInitializer());
+
+            Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false)
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
